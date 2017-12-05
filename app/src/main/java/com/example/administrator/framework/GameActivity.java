9049d1198859d27508a7dca1359b97b52a4173d5
RandomlyChristen <33441923+RandomlyChristen@users.noreply.github.com>
2017-12-05 13:17:11 +0900

    아시발몰라
    아시발몰라

---
+++ app/src/main/java/com/example/administrator/framework/GameActivity.java
@@ -0,0 +1,88 @@
+package com.example.administrator.framework;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.example.administrator.networks.ClientWork;
+import com.example.administrator.networks.ServerWork;
+
+/**
+ * Created by Administrator on 2017-11-27.
+ */
+
+public class GameActivity extends Activity {
+    GameView gameView;
+    public static double size;
+
+
+    /* TODO 네트워크
+    static final int gamePort = 1451;
+
+    private String myName;
+    private String myIP;
+    private String opponentName;
+    private String opponentIP;
+    private String isHost;
+
+    private ServerWork serverWork;
+    private ClientWork clientWork;
+    */
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //디스플레이의 해상도를 구함
+        DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+        int width = dm.widthPixels;
+        size = (double)width/1920.0;
+
+        gameView = new GameView(this);
+        setContentView(gameView);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+        /* TODO 네트워크
+        myName = getIntent().getStringExtra("myName");
+        myIP = getIntent().getStringExtra("myIP");
+        opponentName = getIntent().getStringExtra("opponentName");
+        opponentIP = getIntent().getStringExtra("opponentIP");
+        isHost = getIntent().getStringExtra("isHost");
+
+        if (isHost.equals("yes")) {
+            serverWork = new ServerWork(myName, myIP, opponentName, opponentIP, gamePort);
+            clientWork = new ClientWork(myIP, gamePort);
+        } else {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            clientWork = new ClientWork(opponentIP, gamePort);
+        }
+        */
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.gameview_thread.setRunnig(false);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.gameview_thread.setRunnig(true);
+    }
+}
